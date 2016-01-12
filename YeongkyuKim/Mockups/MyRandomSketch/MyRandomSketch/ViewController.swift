1198f79da1c57307c70ea5b427e843c5860706d2
karl6885 <ITGlobal10@cscoi010ui-iMac.local>
2016-01-12 14:49:43 +0900

    변경

---
+++ YeongkyuKim/Mockups/MyRandomSketch/MyRandomSketch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyRandomSketch
+//
+//  Created by 특강 on 2016. 1. 11..
+//  Copyright © 2016년 KoreaUniv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
