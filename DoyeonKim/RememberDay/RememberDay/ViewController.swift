d5fe228c195a3bb73e6627412799a7ec54684d7e
doken111 <doken@DOKENui-MacBook-Pro.local>
2016-01-25 14:55:44 +0900

    Update Code

---
+++ DoyeonKim/RememberDay/RememberDay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RememberDay
+//
+//  Created by DOKEN on 2016. 1. 20..
+//  Copyright © 2016년 DOKEN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
