e1a00193f5f0f44a167781c4b9fa1cbccccabe43
Jungmin Yu <soomini@yusumin-ui-MacBook-Pro.local>
2016-01-26 10:38:52 +0900

    파일교체
    Xcode 파일교

---
+++ JungminYu/Mockups/ContestExhibitPractice/ContestExhibitPracticeTests/ContestExhibitPracticeTests.m
@@ -0,0 +1,39 @@
+//
+//  ContestExhibitPracticeTests.m
+//  ContestExhibitPracticeTests
+//
+//  Created by 유수민 on 2016. 1. 12..
+//  Copyright © 2016년 JungminYu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ContestExhibitPracticeTests : XCTestCase
+
+@end
+
+@implementation ContestExhibitPracticeTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
