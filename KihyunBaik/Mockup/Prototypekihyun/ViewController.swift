ea544aa219e5d13017c68539c9095f9ac4ac4f95
skyshow2 <kihyun-soyeon@Kihyun-Soyeonui-MacBook-Air.local>
2016-01-26 11:50:14 +0900

    사진폴더추가

---
+++ KihyunBaik/Mockup/Prototypekihyun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Prototypekihyun
+//
+//  Created by Kihyun-Soyeon on 2016. 1. 19..
+//  Copyright © 2016년 Kihyun-Soyeon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
