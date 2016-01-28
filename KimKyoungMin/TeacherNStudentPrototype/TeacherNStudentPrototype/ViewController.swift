16dc8c4088ccd594548f0c2fc59bac3b191cd6bf
Dr.KKM <muktop@korea.ac.kr>
2016-01-28 15:55:30 +0900

    TeacherNStrudent Initial Commit

---
+++ KimKyoungMin/TeacherNStudentPrototype/TeacherNStudentPrototype/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TeacherNStudentPrototype
+//
+//  Created by cscoi007 on 2016. 1. 28..
+//  Copyright © 2016년 KoreaUniversity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
