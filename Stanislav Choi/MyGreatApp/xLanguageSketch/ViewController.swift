b5097a5b5d0f1d3181c03d8a543a5470ecd304e3
Stanislav Choi <statelo@naver.com>
2016-02-15 14:59:03 +0900

    More Information Added To My Folder

---
+++ Stanislav Choi/MyGreatApp/xLanguageSketch/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  xLanguageSketch
+//
+//  Created by cscoi024 on 2016. 1. 28..
+//  Copyright © 2016년 stanislav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
