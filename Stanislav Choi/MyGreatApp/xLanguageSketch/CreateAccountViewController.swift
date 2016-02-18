c26e663e573b0c617ae92b46fa1d997a60affb0b
Stanislav Choi <statelo@naver.com>
2016-02-18 16:26:16 +0900

    Collection View Added

---
+++ Stanislav Choi/MyGreatApp/xLanguageSketch/CreateAccountViewController.swift
@@ -0,0 +1,41 @@
+//
+//  CreateAccountViewController.swift
+//  xLanguageSketch
+//
+//  Created by cscoi024 on 2016. 2. 18..
+//  Copyright © 2016년 stanislav. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func cancelButton(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Stanislav Choi/MyGreatApp/xLanguageSketch/FriendsCollectionViewController.swift
@@ -0,0 +1,45 @@
+//
+//  FriendsCollectionViewController.swift
+//  xLanguageSketch
+//
+//  Created by cscoi024 on 2016. 2. 18..
+//  Copyright © 2016년 stanislav. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+var Friends = [String]()
+
+class FriendsCollectionViewController: UICollectionViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        Friends = ["Leonard", "Sheldon", "Howard", "Rajesh", "Penny", "Amy", "Stanislav", "Irina", "Bernadette"]
+        
+        self.collectionView?.backgroundColor = UIColor.whiteColor()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return Friends.count
+    }
+
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath) as UICollectionViewCell
+        
+        let Label = cell.viewWithTag(1) as! UILabel
+        
+        Label.text = Friends[indexPath.row]
+        
+        let Button = cell.viewWithTag(2) as! UIButton
+        
+        Button.setTitle("Information", forState: UIControlState.Normal)
+        
+        return cell
+    }
+}
